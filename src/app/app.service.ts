e2f4b374dc10d350262374447441e4ebc00383ee
niravrathod <niravrathod473@gmail.com>
2022-07-29 18:19:19 +0530

    1 file modified

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
+import { Observable } from 'rxjs';
+import { Observer } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import * as socketIo from 'socket.io-client';
 import { Socket } from './interfaces';
